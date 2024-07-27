26ce5058c891ea6dbeb729784b90f1c39c610081
Manish19509 <manishkeshri19509@gmail.com>
2024-07-27 13:08:49 +0530

    commit

---
+++ 5CharArrayString.cpp/1charArrayString.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    // creation
+    char ch[10];
+
+    // insertion
+    cin >> ch;
+
+    // print
+    cout << "Printing the value of ch : " << ch;
+    return 0;
+}
